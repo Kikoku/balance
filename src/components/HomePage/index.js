e5d70d837e3f368a3ff86662b3e3e7a77dab97e4
Collin <candrew022@gmail.com>
2017-02-10 15:34:10 -0500

    Setup Routes

---
+++ src/components/HomePage/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {
+  Container,
+  Row
+} from '../Grid';
+
+const HomePage = () => (
+  <Container>
+    <Row>
+      <div className="col-xs-12">
+        <h2>
+          Home
+        </h2>
+      </div>
+    </Row>
+  </Container>
+)
+
+export default HomePage;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {
+  Router,
+  browserHistory
+} from 'react-router';
+import Routes from './Routes';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <Router
+    routes={Routes}
+    history={browserHistory}
+  />,
   document.getElementById('root')
 );

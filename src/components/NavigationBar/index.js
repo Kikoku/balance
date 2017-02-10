e931ed83df6a82152880adb5fde1f4b373057b09
Collin <candrew022@gmail.com>
2017-02-10 15:45:08 -0500

    Create Navigation Bar

---
+++ src/components/NavigationBar/index.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+import Icon from '../Icon';
+import { Link } from 'react-router';
+import logo from '../../balance.png';
+import {
+  Container,
+} from '../Grid';
+
+class NavigationBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      collapse: true
+    }
+  }
+  toggleExpand() {
+    this.setState({
+      collapse: !this.state.collapse
+    })
+  }
+  render() {
+    return (
+      <Container fluid="true">
+        <nav className="navbar navbar-inverse navbar-collapse navbar-fixed-top">
+          <div className="container-fluid">
+            <div className="navbar-header">
+              <button className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" onClick={()=>this.toggleExpand()}>
+                <span className="icon-bar"/>
+                <span className="icon-bar"/>
+                <span className="icon-bar"/>
+              </button>
+              <Link className="navbar-brand" to="/">
+                <img src={logo} role="presentation"/>&nbsp;Balance
+              </Link>
+            </div>
+             <div id="navbar" className={`navbar-collapse collapse ${this.state.collapse ? '' : 'in'}`}>
+              <ul className="nav navbar-nav navbar-right">
+                <li className={this.props.location.pathname === '/venues' ? 'active' : ''}>
+                  <Link to="/venues">
+                    <Icon icon="fort-awesome"/> Venues
+                  </Link>
+                </li>
+                <li className={this.props.location.pathname === '/leagues' ? 'active' : ''}>
+                  <Link to="/leagues">
+                    <Icon icon="tasks" /> Leagues
+                  </Link>
+                </li>
+                <li className={this.props.location.pathname === '/events' ? 'active' : ''}>
+                  <Link to="/events">
+                    <Icon icon="calendar" /> Events
+                  </Link>
+                </li>
+                <li className={this.props.location.pathname === '/dashboard' ? 'active' : ''}>
+                  <Link to="/dashboard">
+                    <Icon icon="dashboard" /> Dashboard
+                  </Link>
+                </li>
+                <li className={this.props.location.pathname === '/login' ? 'active' : ''}>
+                  <Link to="/login">
+                    <Icon icon="lock" /> Login
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </Container>
+    )
+  }
+}
+
+export default NavigationBar;

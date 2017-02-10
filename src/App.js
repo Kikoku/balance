e17b7d97979b9a5d196da017ca1bb33f955053a3
Collin <candrew022@gmail.com>
2017-02-10 15:45:38 -0500

    Add NavBar to App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import NavigationBar from './components/NavigationBar';
 
 class App extends Component {
   render() {
     return (
       <div>
+        <NavigationBar location={this.props.location}/>
         {this.props.children}
       </div>
     );

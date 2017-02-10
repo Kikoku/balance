78fb141362f46af17b99aaef1713c65168af050f
Collin <candrew022@gmail.com>
2017-02-10 17:28:00 -0500

    Link to pages

--- src/Routes.js
+++ src/Routes.js
@@ -5,10 +5,20 @@ import {
 } from 'react-router';
 
 import App from './App';
-import HomePage from './components/HomePage'
+import HomePage from './components/HomePage';
+import VenuesPage from './components/VenuesPage';
+import LeaguesPage from './components/LeaguesPage';
+import EventsPage from './components/EventsPage';
+import DashboardPage from './components/DashboardPage'
+import LoginPage from './components/LoginPage'
 
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={HomePage}/>
+    <Route path="venues" component={VenuesPage} />
+    <Route path="leagues" component={LeaguesPage} />
+    <Route path="events" component={EventsPage} />
+    <Route path="dashboard" component={DashboardPage} />
+    <Route path="login" component={LoginPage} />
   </Route>
 )

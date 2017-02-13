2f7c143d65e9cc86b05f7be6900429091e53daa8
Collin <candrew022@gmail.com>
2017-02-13 07:57:58 -0500

    setup apollo

--- src/index.js
+++ src/index.js
@@ -6,11 +6,39 @@ import {
 } from 'react-router';
 import Routes from './Routes';
 import './index.css';
+import ApolloClient, {
+  createNetworkInterface
+} from 'apollo-client';
+import {
+  ApolloProvider,
+} from 'react-apollo';
+
+const networkInterface = createNetworkInterface({
+  uri: 'http://localhost:8080'
+})
+
+networkInterface.use([{
+  applyMiddleware(req, next) {
+    if(!req.options.headers) {
+      req.options.headers = {};
+    }
+
+    const token = localStorage.getItem('access_token');
+    req.options.authorization = token ? token : null;
+    next();
+  }
+}])
+
+const client = new ApolloClient({
+  networkInterface
+});
 
 ReactDOM.render(
-  <Router
-    routes={Routes}
-    history={browserHistory}
-  />,
+  <ApolloProvider client={client}>
+    <Router
+      routes={Routes}
+      history={browserHistory}
+    />
+  </ApolloProvider>,
   document.getElementById('root')
 );

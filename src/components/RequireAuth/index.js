f75932a534cd456fa7ebee4a3f7f51d7bdbc5cb3
Collin <candrew022@gmail.com>
2017-02-13 16:57:13 -0500

    Create Auth requirements for pages

---
+++ src/components/RequireAuth/index.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import {
+  graphql
+} from 'react-apollo';
+import gql from 'graphql-tag';
+
+export default (ComposedComponent, AuthScheme = 'admin') => {
+  class Authentication extends Component {
+
+    componentWillUpdate(nextProps) {
+      switch (AuthScheme) {
+        case 'admin':
+          if(nextProps.data.viewer.roles.findIndex((role) => role.name === 'admin') === -1) {
+            nextProps.router.push('/login')
+          }
+          break;
+        case 'organization':
+          if(nextProps.data.viewer.roles.findIndex((role) => role.name === 'admin') === -1) {
+            nextProps.router.push('/login')
+          }
+          break;
+        case 'guest':
+          if(nextProps.data.viewer.roles.findIndex((role) => role.name === 'guest') === -1) {
+
+            nextProps.router.push('/')
+          }
+          break;
+        default:
+
+      }
+
+    }
+
+    render() {
+      return (
+        <ComposedComponent {...this.props} />
+      )
+    }
+  }
+
+  const Viewer = gql`query Viewer { viewer { name, roles {name}} }`
+
+  const componentWithData = graphql(Viewer)(Authentication);
+
+  return componentWithData;
+
+}

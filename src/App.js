85e35e92f1ca376a3cdbed4cb1939da5ef2f8c26
Collin <candrew022@gmail.com>
2017-02-13 07:58:50 -0500

    create base query

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+import {
+  graphql
+} from 'react-apollo';
+import gql from 'graphql-tag';
 import './App.css';
 import NavigationBar from './components/NavigationBar';
 
@@ -6,11 +10,18 @@ class App extends Component {
   render() {
     return (
       <div>
-        <NavigationBar location={this.props.location}/>
+        <NavigationBar
+          location={this.props.location}
+          viewer={this.props.data.viewer}
+        />
         {this.props.children}
       </div>
     );
   }
 }
 
-export default App;
+const MyQuery = gql`query MyQuery { viewer { name, roles {name}} }`
+
+const componentWithData = graphql(MyQuery)(App);
+
+export default componentWithData;

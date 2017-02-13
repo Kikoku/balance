f3efb614ad9854930c51154466df0243e3283ce9
Collin <candrew022@gmail.com>
2017-02-13 16:55:35 -0500

    Collapse nav menu on navigation

--- src/components/NavigationBar/index.js
+++ src/components/NavigationBar/index.js
@@ -13,18 +13,25 @@ class NavigationBar extends Component {
       collapse: true
     }
   }
-  _toggleExpand() {
+  _collapse() {
     this.setState({
-      collapse: !this.state.collapse
+      collapse: true
+    })
+  }
+  _expand() {
+    this.setState({
+      collapse: false
     })
   }
   render() {
+    let { viewer } = this.props;
+    let guest = viewer ? viewer.roles.findIndex((role) => role.name === 'guest') > -1 : true;
     return (
       <Container fluid="true">
         <nav className="navbar navbar-inverse navbar-collapse navbar-fixed-top">
           <div className="container-fluid">
             <div className="navbar-header">
-              <button className="navbar-toggle collapsed" data-target="#navbar" onClick={()=>this._toggleExpand()}>
+              <button className="navbar-toggle collapsed" onClick={ this.state.collapse ? () => this._expand() : () => this._collapse()}>
                 <span className="icon-bar"/>
                 <span className="icon-bar"/>
                 <span className="icon-bar"/>
@@ -35,45 +42,47 @@ class NavigationBar extends Component {
             </div>
              <div id="navbar" className={`navbar-collapse collapse ${this.state.collapse ? '' : 'in'}`}>
               <ul className="nav navbar-nav navbar-right">
-                <li style={{color: 'white'}}>
-                  {this.props.viewer ? this.props.viewer.name : ''}
+                <li className={this.props.location.pathname === '/user' ? 'active' : ''} onClick={() => this._collapse()}>
+                  <Link to="/user" disabled={ guest ? true : null}>
+                  <Icon icon="user"/> {viewer ? viewer.name : null}
+                  </Link>
                 </li>
-                <li className={this.props.location.pathname === '/venues' ? 'active' : ''}>
+                <li className={this.props.location.pathname === '/venues' ? 'active' : ''} onClick={() => this._collapse()}>
                   <Link to="/venues">
                     <Icon icon="fort-awesome"/> Venues
                   </Link>
                 </li>
-                <li className={this.props.location.pathname === '/leagues' ? 'active' : ''}>
+                <li className={this.props.location.pathname === '/leagues' ? 'active' : ''} onClick={() => this._collapse()}>
                   <Link to="/leagues">
                     <Icon icon="tasks" /> Leagues
                   </Link>
                 </li>
-                <li className={this.props.location.pathname === '/events' ? 'active' : ''}>
+                <li className={this.props.location.pathname === '/events' ? 'active' : ''} onClick={() => this._collapse()}>
                   <Link to="/events">
                     <Icon icon="calendar" /> Events
                   </Link>
                 </li>
                 {
-                  this.props.viewer && this.props.viewer.roles.includes({name: 'guest'}) ?
-                    <li className={this.props.location.pathname === '/dashboard' ? 'active' : ''}>
+                  guest ?
+                    null
+                  :
+                    <li className={this.props.location.pathname === '/dashboard' ? 'active' : ''} onClick={() => this._collapse()}>
                       <Link to="/dashboard">
                         <Icon icon="dashboard" /> Dashboard
                       </Link>
                     </li>
-                  :
-                    ''
                 }
                 {
-                  this.props.viewer && this.props.viewer.roles.includes({name: 'guest'}) ?
-                    <li className={this.props.location.pathname === '/login' ? 'active' : ''}>
+                  guest ?
+                    <li className={this.props.location.pathname === '/login' ? 'active' : ''} onClick={() => this._collapse()}>
                       <Link to="/login">
-                        <Icon icon="unlock-alt" /> Logout
+                        <Icon icon="lock" /> Login
                       </Link>
                     </li>
                   :
-                    <li className={this.props.location.pathname === '/login' ? 'active' : ''}>
+                    <li className={this.props.location.pathname === '/login' ? 'active' : ''} onClick={() => this._collapse()}>
                       <Link to="/login">
-                        <Icon icon="lock" /> Login
+                        <Icon icon="unlock-alt" /> Logout
                       </Link>
                     </li>
                 }

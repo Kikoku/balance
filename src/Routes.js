f716461fbe4b03de2acb8b4a09897d40039f5b7f
Collin <candrew022@gmail.com>
2017-02-14 14:35:38 -0500

    Setup routing to single Venue

--- src/Routes.js
+++ src/Routes.js
@@ -9,6 +9,8 @@ import RequireAuth from './components/RequireAuth';
 import App from './App';
 import HomePage from './components/HomePage';
 import VenuesPage from './components/VenuesPage';
+import VenuesContainer from './components/VenuesContainer';
+import VenueContainer from './components/VenueContainer';
 import LeaguesPage from './components/LeaguesPage';
 import EventsPage from './components/EventsPage';
 import DashboardPage from './components/DashboardPage'
@@ -17,7 +19,10 @@ import LoginPage from './components/LoginPage'
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={HomePage}/>
-    <Route path="venues" component={VenuesPage} />
+    <Route path="venues" component={VenuesPage}>
+      <IndexRoute component={VenuesContainer} />
+      <Route path="/venues/:venueId" component={VenueContainer} />
+    </Route>
     <Route path="leagues" component={LeaguesPage} />
     <Route path="events" component={EventsPage} />
     <Route path="dashboard" component={RequireAuth(DashboardPage, 'organization')} />

d1dd8438e66ee331b81547a86801413802f23448
Collin <candrew022@gmail.com>
2017-02-14 12:50:14 -0500

    Create VenuesContainer component

---
+++ src/components/VenuesContainer/index.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import {
+  Panel,
+  PanelHeading,
+} from '../Grid';
+import {
+  graphql,
+} from 'react-apollo';
+import gql from 'graphql-tag';
+import VenueNode from '../VenueNode';
+
+const VenuesContainer = ({ data }) => {
+  if (data.viewer) {
+    console.log(data.viewer);
+  }
+  return (
+  <Panel>
+    <PanelHeading>
+
+    </PanelHeading>
+    <table className="table">
+      <tbody>
+        <tr>
+          <th>
+            Name
+          </th>
+          <th>
+            Email
+          </th>
+        </tr>
+        {data.loading ? <tr><td>Loading</td></tr> : null }
+        {data.error ? <tr><td>ERROR</td></tr> : null}
+        {
+          data.viewer ? data.viewer.organizations.edges.map((organization, key) => <VenueNode key={key} organization={organization.node} />) : null
+        }
+      </tbody>
+    </table>
+  </Panel>
+)}
+
+const Venues = gql`query Venues {
+  viewer {
+    organizations {
+      edges {
+        node {
+          ...VenuesContainerVenue
+        }
+      }
+    }
+  }
+}
+${VenueNode.fragments.venue}`
+
+console.log(Venues);
+
+const componentWithData = graphql(Venues)(VenuesContainer);
+
+export default componentWithData;

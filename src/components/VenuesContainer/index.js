b6defd9fdfb8c02a6a68315e6acd8b91a73ded24
Collin <candrew022@gmail.com>
2017-02-14 14:35:27 -0500

    Nest elements into VenuesContainer

--- src/components/VenuesContainer/index.js
+++ src/components/VenuesContainer/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import {
   Panel,
   PanelHeading,
+  Row
 } from '../Grid';
+import Icon from '../Icon';
 import {
   graphql,
 } from 'react-apollo';
@@ -10,28 +12,38 @@ import gql from 'graphql-tag';
 import VenueNode from '../VenueNode';
 
 const VenuesContainer = ({ data }) => (
-  <Panel>
-    <PanelHeading>
+  <Row>
+    <div className="col-xs-12">
+      <h2>
+        <Icon icon="fort-awesome"/> Venues
+      </h2>
+    </div>
+    <div className="col-xs-12">
+      <Panel>
+        <PanelHeading>
 
-    </PanelHeading>
-    <table className="table">
-      <tbody>
-        <tr>
-          <th>
-            Name
-          </th>
-          <th>
-            Email
-          </th>
-        </tr>
-        {data.loading ? <tr><td>Loading</td></tr> : null }
-        {data.error ? <tr><td>ERROR</td></tr> : null}
-        {
-          data.viewer ? data.viewer.organizations.edges.map((organization, key) => <VenueNode key={key} organization={organization.node} />) : null
-        }
-      </tbody>
-    </table>
-  </Panel>
+        </PanelHeading>
+        <table className="table">
+          <tbody>
+            <tr>
+              <th>
+                Name
+              </th>
+              <th>
+                Email
+              </th>
+            </tr>
+            {data.loading ? <tr><td>Loading</td></tr> : null }
+            {data.error ? <tr><td>ERROR</td></tr> : null}
+            {
+              data.viewer ? data.viewer.organizations.edges.map((organization, key) => <VenueNode key={key} organization={organization.node} />) : null
+            }
+          </tbody>
+        </table>
+      </Panel>
+    </div>
+  </Row>
+
 )
 
 const Venues = gql`query Venues {
--- src/components/VenuesPage/index.js
+++ src/components/VenuesPage/index.js
@@ -1,23 +1,12 @@
 import React from 'react';
 import {
   Container,
-  Row
 } from '../Grid';
-import Icon from '../Icon';
 import VenuesContainer from '../VenuesContainer';
 
-const VenuesPage = () => (
+const VenuesPage = ({children}) => (
   <Container>
-    <Row>
-      <div className="col-xs-12">
-        <h2>
-          <Icon icon="fort-awesome"/> Venues
-        </h2>
-      </div>
-      <div className="col-xs-12">
-        <VenuesContainer />
-      </div>
-    </Row>
+    {children}
   </Container>
 )
 

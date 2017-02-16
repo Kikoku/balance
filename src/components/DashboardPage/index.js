b1bb8afbda7d5620910e757f0795c7a200fa0bbe
Collin <candrew022@gmail.com>
2017-02-16 15:51:50 -0500

    Simplify DashboardPage

--- src/components/DashboardPage/index.js
+++ src/components/DashboardPage/index.js
@@ -1,20 +1,10 @@
 import React from 'react';
 import {
   Container,
-  Row
 } from '../Grid';
-import Icon from '../Icon';
 
-const DashboardPage = () => (
-  <Container>
-    <Row>
-      <div className="col-xs-12">
-        <h2>
-          <Icon icon="dashboard"/> Dashboard
-        </h2>
-      </div>
-    </Row>
-  </Container>
+const DashboardPage = ({ children, data }) => (
+  <Container children={children} data={data} />
 )
 
 export default DashboardPage;

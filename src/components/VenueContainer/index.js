18c859355174b32c11f20ce55c4d6d2311189548
Collin <candrew022@gmail.com>
2017-02-28 16:07:29 -0500

    Add Leagues to Organization View

--- src/components/VenueContainer/index.js
+++ src/components/VenueContainer/index.js
@@ -14,44 +14,62 @@ import VenueNode from '../VenueNode';
 import {
   Link,
 } from 'react-router';
+import LeagueNode from '../LeagueNode';
+import LeaguesPanel from '../LeaguesPanel';
 
-class VenueContainer extends React.Component {
-  render() {
-    return (
-      <Row>
-        <div className="col-sm-12">
-          <h2>
-            <Icon icon="fort-awesome"/> <Link to="/venues">Venues</Link>
-            {
-              !this.props.data.loading ?
-                ` / ${this.props.data.viewer.organization.name}`
-              :
-                <i className="fa fa-spinner fa-spin fa-3x fa-fw></i>" />
+const VenueContainer = ({ data }) => {
+  console.log('data');
+  console.log(data);
+  return (
+  <Row>
+    <div className="col-sm-12">
+      <h2>
+        <Icon icon="fort-awesome"/> <Link to="/venues">Venues</Link>
+        {
+          !data.loading ?
+            ` / ${data.viewer.organization.name}`
+          :
+            <i className="fa fa-spinner fa-spin fa-3x fa-fw></i>" />
+        }
+      </h2>
+    </div>
+    <div className="col-sm-12">
+      <Panel>
+        <PanelHeading>
+        </PanelHeading>
+        <PanelBody>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </PanelBody>
+      </Panel>
+    </div>
+    <div className="col-sm-12">
+      <LeaguesPanel
+        loading={data.loading}
+        error={data.error}
+        viewer={data.viewer}
+      />
+    </div>
+  </Row>
+)}
+
+const Venue = gql`
+  query Venue($id: ID!) {
+    viewer {
+      organization(id: $id) {
+        ...VenuesContainerVenue
+        leagues {
+          edges {
+            node {
+              ...LeaguesContainerLeague
             }
-          </h2>
-        </div>
-        <div className="col-sm-12">
-          <Panel>
-            <PanelHeading>
-            </PanelHeading>
-            <PanelBody>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </PanelBody>
-          </Panel>
-        </div>
-      </Row>
-    )
-  }
-}
-
-const Venue = gql`query Venue($id: ID!) {
-  viewer {
-    organization(id: $id) {
-      ...VenuesContainerVenue
+          }
+        }
+      }
     }
   }
-}
-${VenueNode.fragments.venue}`
+  ${VenueNode.fragments.venue}
+  ${LeagueNode.fragments.league}
+`
 
 export default graphql(Venue, {
   options: ({ routeParams: { venueId } }) => ({

2c956a83cacd6fb2a0db8c07c46e2de40e75e61f
Collin <candrew022@gmail.com>
2017-02-28 11:04:02 -0500

    Add relational components

--- src/components/LeagueContainer/index.js
+++ src/components/LeagueContainer/index.js
@@ -3,7 +3,9 @@ import {
   Panel,
   PanelHeading,
   PanelBody,
-  Row
+  Row,
+  ListGroup,
+  ListGroupItem,
 } from '../Grid';
 import Icon from '../Icon';
 import {
@@ -11,20 +13,24 @@ import {
 } from 'react-apollo';
 import gql from 'graphql-tag';
 import LeagueNode from '../LeagueNode';
+import LeagueEventNode from '../LeagueEventNode';
+import LeagueResultNode from '../LeagueResultNode';
 import {
   Link,
 } from 'react-router';
 
 class LeagueContainer extends React.Component {
   render() {
+    console.log(this.props);
+    const { loading, viewer } = this.props.data;
     return (
       <Row>
         <div className="col-sm-12">
           <h2>
             <Icon icon="tasks"/> <Link to="/leagues">Leagues</Link>
             {
-              !this.props.data.loading ?
-                ` / ${this.props.data.viewer.league.title}`
+              !loading ?
+                ` / ${viewer.league.title}`
               :
                 <i className="fa fa-spinner fa-spin fa-3x fa-fw></i>" />
             }
@@ -39,6 +45,68 @@ class LeagueContainer extends React.Component {
             </PanelBody>
           </Panel>
         </div>
+        <div className="col-sm-12">
+          <Panel>
+            <PanelHeading>
+              <Icon icon="sort-numeric-asc"/> Results
+            </PanelHeading>
+            <ListGroup>
+              <ListGroupItem>
+                <Row>
+                  <div className="col-sm-4 col-xs-6">
+                    Player
+                  </div>
+                  <div className="col-sm-4 hidden-xs">
+                    Win | Loss | Draw
+                  </div>
+                  <div className="col-sm-4 col-xs-6">
+                    Elo
+                  </div>
+                </Row>
+              </ListGroupItem>
+              {
+                !loading
+                ?
+                  viewer.league.users.edges.map(user => <LeagueResultNode {...user.node}/> )
+                :
+                  <ListGroupItem>
+                    <i className="fa fa-spinner fa-spin fa-3x fa-fw></i>" />
+                  </ListGroupItem>
+              }
+            </ListGroup>
+          </Panel>
+        </div>
+        <div className="col-sm-12">
+          <Panel>
+            <PanelHeading>
+              <Icon icon="calendar"/> Events
+            </PanelHeading>
+            <ListGroup>
+              <ListGroupItem>
+                <Row>
+                  <div className="col-sm-4 col-xs-6">
+                    Event
+                  </div>
+                  <div className="col-sm-4 hidden-xs">
+                    Date
+                  </div>
+                  <div className="col-sm-4 col-xs-6">
+                    Venue
+                  </div>
+                </Row>
+              </ListGroupItem>
+              {
+                !loading
+                ?
+                  viewer.league.events.edges.map(event => <LeagueEventNode {...event.node} key={event.node.id}/> )
+                :
+                  <ListGroupItem>
+                    <i className="fa fa-spinner fa-spin fa-3x fa-fw></i>" />
+                  </ListGroupItem>
+              }
+            </ListGroup>
+          </Panel>
+        </div>
       </Row>
     )
   }
@@ -49,10 +117,26 @@ const League = gql`
     viewer {
       league(id: $id) {
         ...LeaguesContainerLeague
+        events {
+          edges {
+            node {
+              ...LeagueEventNode
+            }
+          }
+        }
+        users {
+          edges {
+            node {
+              ...LeagueResultNode
+            }
+          }
+        }
       }
     }
   }
   ${LeagueNode.fragments.league}
+  ${LeagueEventNode.fragments.events}
+  ${LeagueResultNode.fragments.results}
 `
 
 export default graphql(League, {

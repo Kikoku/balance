6191ab64e87ade7e54f9ea4372124df2bfd51591
Collin <candrew022@gmail.com>
2017-02-28 15:24:17 -0500

    Create ResultChangeFlag Component

---
+++ src/components/ResultChangeFlag/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Icon from '../Icon';
+
+const ResultChangeFlag = ({change}) => {
+    return (
+      <span>
+        {
+          change != 0
+          ?
+            <span style={{color: change > 0 ? '#27ae60' : '#c0392b'}}>
+              <Icon icon={change > 0 ? 'chevron-up' : 'chevron-down'}/> {change}
+            </span>
+          :
+            <Icon icon="minus" />
+        }
+      </span>
+    )
+}
+
+export default ResultChangeFlag;

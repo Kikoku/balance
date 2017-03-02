db82528f01931045c9c7ee7f68e8ed0d6e802a2b
Collin <candrew022@gmail.com>
2017-03-02 00:45:00 -0500

    Add Icons to Dashboard Nav

--- src/components/DashboardPage/DashboardNav.js
+++ src/components/DashboardPage/DashboardNav.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import {
   Link,
-} from 'react-router'
+} from 'react-router';
+import Icon from '../Icon';
 
 const DashboardNav = ({ location, viewer }) => {
   let admin = viewer ? viewer.roles.findIndex((role) => role.name === 'admin') > -1 : false;
@@ -9,7 +10,7 @@ const DashboardNav = ({ location, viewer }) => {
   <ul className="nav nav-pills nav-stacked well">
     <li className={location.pathname === '/dashboard' ? 'active' : null}>
       <Link to="/dashboard">
-        Dashboard
+        <Icon icon="dashboard"/> Dashboard
       </Link>
     </li>
     {
@@ -17,7 +18,7 @@ const DashboardNav = ({ location, viewer }) => {
       ?
         <li className={location.pathname.includes('new-organization') ? 'active' : null}>
           <Link to="/dashboard/new-organization">
-            New Organization
+            <Icon icon="fort-awesome"/> New Organization
           </Link>
         </li>
       :
@@ -25,12 +26,12 @@ const DashboardNav = ({ location, viewer }) => {
     }
     <li className={location.pathname.includes('new-league') ? 'active' : null}>
       <Link to="/dashboard/new-league">
-        New Leauge
+        <Icon icon="tasks"/> New Leauge
       </Link>
     </li>
     <li className={location.pathname.includes('new-event') ? 'active' : null}>
       <Link to="/dashboard/new-event">
-        New Event
+        <Icon icon="calendar"/> New Event
       </Link>
     </li>
   </ul>

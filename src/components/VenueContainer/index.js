6d4d40d95e975b10440337a5c34b5c02d6774df8
Collin <candrew022@gmail.com>
2017-03-02 00:42:39 -0500

    Remove filler text

--- src/components/VenueContainer/index.js
+++ src/components/VenueContainer/index.js
@@ -32,15 +32,6 @@ const VenueContainer = ({ data }) => (
       </h2>
     </div>
     <div className="col-sm-12">
-      <Panel>
-        <PanelHeading>
-        </PanelHeading>
-        <PanelBody>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </PanelBody>
-      </Panel>
-    </div>
-    <div className="col-sm-12">
       <LeaguesPanel
         loading={data.loading}
         error={data.error}

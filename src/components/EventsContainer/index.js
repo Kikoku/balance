46f22083988a6d77a7cc30e70206fbc5a1b058e5
Collin <candrew022@gmail.com>
2017-03-22 15:38:19 -0400

    Add Pagination to Events

--- src/components/EventsContainer/index.js
+++ src/components/EventsContainer/index.js
@@ -10,9 +10,9 @@ import {
 } from 'react-apollo';
 import gql from 'graphql-tag';
 import EventNode from '../EventNode';
+import update from 'immutability-helper';
 
-const EventsContainer = ({ data }) => {
-  return (
+const EventsContainer = ({ data, loadMoreEntries }) => (
   <Row>
     <div className="col-sm-12">
       <h2>
@@ -42,24 +42,77 @@ const EventsContainer = ({ data }) => {
             {
               data.viewer ? data.viewer.events.edges.map((event, key) => <EventNode key={key} event={event.node} />) : null
             }
+            {
+              data.viewer ? data.viewer.events.pageInfo.hasNextPage ? <tr>
+                <td
+                  colSpan={3}
+                  style={{
+                    textAlign: 'center'
+                  }}
+                  onClick={loadMoreEntries}
+                >
+                  <span className="btn btn-primary">
+                    Load More...
+                  </span>
+                </td>
+              </tr>: null : null
+            }
           </tbody>
         </table>
       </Panel>
     </div>
   </Row>
-)}
+)
 
-const Events = gql`query Events {
-  viewer {
-    events {
-      edges {
-        node {
-          ...EventsContainerEvent
+const Events = gql`
+  query Events($cursor: String) {
+    viewer {
+      events(first:10, after: $cursor) {
+        edges {
+          node {
+            ...EventsContainerEvent
+          }
+        }
+        pageInfo{
+          endCursor
+          hasNextPage
         }
       }
     }
   }
-}
-${EventNode.fragments.event}`
+  ${EventNode.fragments.event}
+`
 
-export default graphql(Events)(EventsContainer);
+export default graphql(Events, {
+  props({ data, data: { loading, viewer, fetchMore } }) {
+    return {
+      data,
+      loadMoreEntries: () => {
+        return fetchMore({
+          query: Events,
+          variables: {
+            cursor: viewer.events.pageInfo.endCursor
+          },
+          updateQuery: (previousResult, { fetchMoreResult }) => {
+            const { edges: newEdges, pageInfo } = fetchMoreResult.data.viewer.events;
+            return update(
+              data,
+              {
+                viewer: {
+                  events: {
+                    edges: {
+                      $push: newEdges
+                    },
+                    pageInfo: {
+                      $set: pageInfo
+                    }
+                  }
+                }
+              }
+            )
+          }
+        })
+      }
+    }
+  }
+})(EventsContainer);

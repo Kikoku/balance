5a036eecb0925207037c76b9164967b5e3461cb3
Collin <candrew022@gmail.com>
2017-03-23 10:43:49 -0400

    Add loadmore pagination to result panel

--- src/components/ResultPanel/index.js
+++ src/components/ResultPanel/index.js
@@ -3,13 +3,14 @@ import {
   Panel,
   PanelHeading,
   ListGroup,
-  ListGroupItem
+  ListGroupItem,
+  Row
 } from '../Grid';
 import Icon from '../Icon';
 import ResultNodeHeading from '../ResultNodeHeading';
 import ResultNode from '../ResultNode';
 
-const ResultPanel = ({ users }) => (
+const ResultPanel = ({ users, loadMoreUsers, hasNextPage }) => (
   <Panel>
     <PanelHeading>
       <Icon icon="sort-numeric-asc"/> Results
@@ -18,6 +19,27 @@ const ResultPanel = ({ users }) => (
       <ResultNodeHeading />
         {
           users.map((user, i) => <ResultNode {...user.node} place={i+1} key={user.node.id}/> )
+        }
+        {
+          hasNextPage ?
+          <ListGroupItem>
+            <Row>
+              <div
+                className="col-md-12"
+                style={{
+                  textAlign: 'center'
+                }}
+              >
+                <span
+                  className="btn btn-primary"
+                  onClick={loadMoreUsers}
+                >
+                  Load More...
+                </span>
+              </div>
+            </Row>
+          </ListGroupItem>
+          : null
         }
     </ListGroup>
   </Panel>
